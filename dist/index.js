1ffea7332511a1a8de24d11b9935b9d50edabbfc
Jan Macku <jamacku@redhat.com>
2023-08-04 14:15:15 +0200

    build: update `dist/`

--- dist/index.js
+++ dist/index.js
@@ -1798,7 +1798,7 @@ var import_graphql = __nccwpck_require__(6442);
 var import_auth_token = __nccwpck_require__(5542);
 
 // pkg/dist-src/version.js
-var VERSION = "4.2.1";
+var VERSION = "4.2.4";
 
 // pkg/dist-src/index.js
 var Octokit = class {
